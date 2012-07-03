3c6f6e13c3aa1e352c5d7b136f05e32f62c7471a
mpilquist <mpilquist@gmail.com>
2012-07-03 00:59:30 -0400

    Support for connecting via -jar jar PID

--- src/main/scala/cjmx/Main.scala
+++ src/main/scala/cjmx/Main.scala
@@ -1,5 +1,8 @@
 package cjmx
 
+import scalaz._
+import Scalaz._
+
 import java.io.PrintWriter
 
 import sbt.{FullReader, LineReader, Path}
@@ -12,25 +15,37 @@ object Main {
   private val historyFile = (userHome / ".cjmx.history").asFile
 
   def main(args: Array[String]) {
+    val consoleReader = new FullReader(Some(historyFile), _: Parser[_])
     val reader = if (args.isEmpty) {
-      new FullReader(Some(historyFile), _: Parser[_])
+      consoleReader
     } else {
-      val reader = readerFromArgs(args)
-      (_: Parser[_]) => reader
+      args.head.parseInt match {
+        case Success(pid) if args.tail.isEmpty =>
+          prefixedReader(Array("connect " + pid), consoleReader)
+        case _ =>
+          constReader(args ++ Array("quit")).liftReader[Parser[_]]
+      }
     }
 
     val statusCode = REPL.run(reader, new PrintWriter(Console.out, true))
     System.exit(statusCode)
   }
 
-  private def readerFromArgs(args: Array[String]): LineReader = {
-    val iter = (args ++ Array("quit")).iterator
+  private def constReader(args: Array[String]): LineReader = {
+    val iter = args.iterator
     new LineReader {
       override def readLine(prompt: String, mask: Option[Char]) =
         if (iter.hasNext) Some(iter.next) else None
     }
   }
 
+  private def prefixedReader(first: Array[String], then: Parser[_] => LineReader): Parser[_] => LineReader = {
+    val firstReader = constReader(first)
+    p => new LineReader {
+      override def readLine(prompt: String, mask: Option[Char]) =
+        firstReader.readLine(prompt, mask) orElse (then(p).readLine(prompt, mask))
+    }
+  }
 
 }
 
