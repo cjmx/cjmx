04a700cebe2e8272b47b1a7c027f04382461874a
mpilquist <mpilquist@gmail.com>
2012-07-13 16:32:21 -0400

    Adding proguard to publish settings

--- project/Build.scala
+++ project/Build.scala
@@ -1,11 +1,19 @@
 import sbt._
 import Keys._
+import ProguardPlugin._
+
 
 object CjmxBuild extends Build {
 
   val toolsJar = Option(Path(Path.fileProperty("java.home").asFile.getParent) / "lib" / "tools.jar").filter { _.exists }.toSeq
 
-  lazy val root = Project(id = "cjmx", base = file("."))
+  lazy val proguardOutputJar = TaskKey[File]("proguard-output-jar")
+
+  lazy val proguardPublishSettings: Seq[Project.Setting[_]] = Seq(
+    proguardOutputJar <<= (proguard, minJarPath) map { (_: Unit, jar: File) => jar }
+  ) ++ addArtifact(Artifact("cjmx", "app"), proguardOutputJar).settings
+
+  lazy val root = Project(id = "cjmx", base = file("."), settings = Defaults.defaultSettings ++ proguardPublishSettings)
 
 }
 
